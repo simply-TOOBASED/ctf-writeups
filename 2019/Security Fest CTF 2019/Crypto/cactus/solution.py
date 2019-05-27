e5dfaa5e67ca0a321ad20149729952e5ef7aa787
Vishwa Iyer <vishwa_iyer@icloud.com>
2019-05-27 08:01:11 -0400

    Update solution.py

--- 2019/Security Fest CTF 2019/Crypto/cactus/solution.py
+++ 2019/Security Fest CTF 2019/Crypto/cactus/solution.py
@@ -2,7 +2,7 @@ import binascii
 
 def solve(o):
     st = []
-    for i in range(8):
+    for i in range(11):
         ss_hex = hex(o >> i)[2:].replace('L', '')
         if len(ss_hex) % 2 == 1:
             ss_hex = '0' + ss_hex
@@ -12,7 +12,6 @@ def solve(o):
 outputs = open("output.txt", "rb").readlines()
 for o in outputs:
     w = solve(long(o))
-    for i in range(8):
+    for i in range(11):
         if "sctf" in w[i]:
             print w[i]
-            break
