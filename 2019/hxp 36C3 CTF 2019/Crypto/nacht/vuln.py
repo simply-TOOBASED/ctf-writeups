b597fd366f5f30bae92b8838010de3550384a945
Vishwa Iyer <vishwa_iyer@icloud.com>
2019-12-30 12:21:41 -0500

    Add files via upload

---
+++ 2019/hxp 36C3 CTF 2019/Crypto/nacht/vuln.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os, ctypes
+
+lib = ctypes.CDLL('./tweetnacl.so')
+
+def mac(key, msg):
+    tag = ctypes.create_string_buffer(16)
+    lib.crypto_onetimeauth_poly1305_tweet(tag, key, len(msg), msg)
+    return bytes(tag)
+
+def chk(key, tag, msg):
+    return not lib.crypto_onetimeauth_poly1305_tweet_verify(tag, key, len(msg), msg)
+
+
+key = os.urandom(32)
+
+for _ in range(32):
+    msg = os.urandom(32)
+    print(msg.hex(), mac(key, msg).hex())
+
+msg = os.urandom(32)
+print(msg.hex(), '?'*32)
+
+tag = bytes.fromhex(input())
+
+if not chk(key, tag, msg):
+    print('no')
+    exit(1)
+
+print(open('flag.txt').read().strip())
+
